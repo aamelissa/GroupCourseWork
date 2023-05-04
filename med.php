30c5aab5dbe6ee8f365841b165ba34bb21ef1831
aamelissa <127861452+aamelissa@users.noreply.github.com>
2023-05-04 14:05:23 +0100

    Add files via upload

--- med.php
+++ med.php
@@ -1,27 +1,27 @@
 <?php
+// Melissa Akinci
+
 
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
-
+session_start();
 // Connect to the database
 try {
     $db = new PDO('sqlite:LocalGPSurgery.db');
 } catch (PDOException $e) {
     die("Error connecting to database: " . $e->getMessage());
 }
-
-// Get the NHS number from the query string
 $nhs = $_GET['nhs'];
-
-// Query the database to get user details
-$query = "SELECT DoseNo as DoseNum, VaccinationDate as vaccinationDate, VaccineManufacturer as vaccineManufacturer, DiseaseTargeted as diseaseTargeted, VaccineType as vaccineType, Product as product, VaccineBatchNumber as vaccineBatchNumber, CountryOfVaccination as countryOfVaccination, Authority as authority, TotalSeriesOfDoses as totalSeriesOfDoses, DisplayName as displayName, SnomedCode as snomedCode, ProcedureCode as procedureCode, Booster as booster, DateEntered as dateEntered FROM MedicalRecords WHERE NHSNumber=:nhs";
+    // Query the database to get user details
+$query = "SELECT DoseNo as DoseNum, VaccinationDate as vaccinationDate, VaccineManufacturer as vaccineManufacturer, DiseaseTargeted as diseaseTargeted, VaccineType as vaccineType, Product as product, VaccineBatchNumber as vaccineBatchNumber, CountryOfVaccination as countryOfVaccination, Authority as authority, TotalSeriesOfDoses as totalSeriesOfDoses, DisplayName as displayName, SNOMEDCode as snomedCode, DateEntered as dateEntered, ProcedureCode as procedureCode, Booster as booster FROM MedicalRecords WHERE NHSNumber = :nhs ORDER BY VaccinationDate ASC";
 $stmt = $db->prepare($query);
 $stmt->bindParam(':nhs', $nhs);
 $stmt->execute();
 
-// Fetch the user details from the database
-$user = $stmt->fetch(PDO::FETCH_ASSOC);
+// Fetch all records as an array
+$records = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-// Return the user details in a JSON format
-echo json_encode($user);
+// Return the records as JSON
+echo json_encode($records);
+
 ?>
